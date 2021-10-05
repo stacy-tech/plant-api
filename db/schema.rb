b8e5e8173ea9d0594c8cca46c2364d27a0a7c1e1
stacy <stacydn23@gmail.com>
2021-10-05 15:19:28 -0400

    structure my comments migration table along with this relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_04_183032) do
+ActiveRecord::Schema.define(version: 2021_10_05_191135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "plant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plant_id"], name: "index_comments_on_plant_id"
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_10_04_183032) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "plants"
 end
