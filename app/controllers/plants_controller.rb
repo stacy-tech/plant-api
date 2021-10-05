f3dd0fa765abd09771611297eebac6fb7bde2ef0
stacy <stacydn23@gmail.com>
2021-10-05 13:24:49 -0400

    structure my create function as well as added my plant params

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,4 +5,17 @@ class PlantsController < ApplicationController
         plants = Plant.all
         render json: plants
     end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant
+    end
+
+    private
+
+    def plant_params
+        params.require(:plant).permit(:name, :difficulty, :light, :water, :image_url)
+    end
+
+
 end
